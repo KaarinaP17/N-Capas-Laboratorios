15c6e33b93bfbc4314b173adaf947be72a039f50
KaarinaP17 <00125123@uca.edu.sv>
2026-05-23 22:02:28 -0600

    chore: initial project setup with base structure and dependencies

---
+++ Laboratorio3/Ejercicio-Hyrule/src/test/java/com/example/ejerciciohyrule/EjercicioHyruleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciohyrule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioHyruleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
