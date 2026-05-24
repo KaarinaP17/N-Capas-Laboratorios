a07f8091c905f98e733a29a9a6ad752f04a9d871
KaarinaP17 <00125123@uca.edu.sv>
2026-05-23 22:00:14 -0600

    chore: initial project setup with base structure and dependencies

---
+++ Laboratorio3/Ejercicio-Hyrule/src/main/java/com/example/ejerciciohyrule/EjercicioHyruleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciohyrule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioHyruleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioHyruleApplication.class, args);
+    }
+
+}
