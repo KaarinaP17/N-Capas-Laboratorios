a5efcc53b038141929a578a7436aacb8be1e91bb
KaarinaP17 <00125123@uca.edu.sv>
2026-06-26 11:58:00 -0600

    feat: prueba de test

---
+++ Laboratorio5/Laboratorio3/Ejercicio-Hyrule/src/main/java/com/example/ejerciciohyrule/dto/response/ApiErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.ejerciciohyrule.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class ApiErrorResponse {
+    private Object message;
+    private int status;
+    private LocalDate time;
+    private String uri;
+}
