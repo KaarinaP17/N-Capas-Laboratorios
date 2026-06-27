e1620eb18520b7e032fc3bf5596f4c560df94c81
KaarinaP17 <00125123@uca.edu.sv>
2026-06-26 22:24:51 -0600

    feat: update packages and another test proof

---
+++ Laboratorio5/Ejercicio-Hyrule/src/main/java/com/example/ejerciciohyrule/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.ejerciciohyrule.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
