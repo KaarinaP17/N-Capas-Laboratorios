2dc7fd39cd17c7cd433e28d98646f716c3a86798
KaarinaP17 <00125123@uca.edu.sv>
2026-06-26 22:25:47 -0600

    feat: update direcctions packages

--- Laboratorio5/Laboratorio3/Ejercicio-Hyrule/src/main/java/com/example/ejerciciohyrule/repository/SpecimenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ejerciciohyrule.repository;
-
-import com.example.ejerciciohyrule.entity.Specimen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SpecimenRepository extends JpaRepository<Specimen, UUID> {
-}
